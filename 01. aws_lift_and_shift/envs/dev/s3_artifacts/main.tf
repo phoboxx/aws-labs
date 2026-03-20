38a8bbfd33d379b962612656b4b4bb8a2e21669c
Frederic Legault <frederic.legault@latticesemi.com>
2026-03-20 11:45:02 -0400

    creating S3 bucket

---
+++ 01. aws_lift_and_shift/envs/dev/s3_artifacts/main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+module "artifact_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "artifact-bucket-aosidjfaosdijfqewklksd"
+}
---
+++ 01. aws_lift_and_shift/modules/s3_iam_role/main.tf
@@ -0,0 +1,7 @@
+# IAM ROLE for app01 to access S3 bucket
+# Prerequisites: s3 bucket with an artifact on it
+
+# Name: s3-admin
+# Role: AmazonS3FullAccess
+
+# Attach role to app01
